17f37538ecdc4d77539b0b48a346dbd2f6f306f5
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-09-07 14:51:24 -0300

    Display DB results - READY

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from "url";
 import path from "path";
 import { title } from "process";
 import { testConnection } from "./src/models/db.js";
+import { getAllOrganizations } from "./src/models/organizations.js";
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
 const PORT = process.env.PORT || 3000;
@@ -23,8 +24,11 @@ app.get("/", async (req, res) => {
     res.render('home', {title});
 });
 app.get("/organizations", async (req, res) => {
-    const title = 'Our Partner Organizations';
-    res.render('organizations', {title});
+    const organizations = await getAllOrganizations();
+    console.log(organizations);
+
+    const title = "Our Partner Organizations";
+    res.render("organizations", { title, organizations });
 });
 app.get("/projects", async (req, res) => {
     const title = 'Service Projects';
@@ -34,6 +38,8 @@ app.get("/categories", async (req, res) => {
     const title = 'Categories';
     res.render('categories', {title});
 });
+
+
 
 //Start up the server.
 app.listen(PORT, async () => {
