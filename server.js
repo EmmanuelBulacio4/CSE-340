bcc04b4f468a0311529b4128b19792fc0953c1f5
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-09-24 00:01:26 -0300

    MVC-controllerPattern

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
 import { testConnection } from "./src/models/db.js";
-import { getAllOrganizations } from "./src/models/organizations.js";
-import { getAllProjects } from "./src/models/projects.js";
-import { getAllCategories } from "./src/models/categories.js";
+import router from "./src/routes.js";
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
 const PORT = process.env.PORT || 3000;
@@ -32,43 +30,8 @@ app.use((req, res, next) => {
     next();
 });
 
-//RUOTES
-app.get("/", async (req, res) => {
-    const title = 'Home';
-    res.render('home', {title});
-});
-
-app.get("/organizations", async (req, res) => {
-    const organizations = await getAllOrganizations();
-    console.log(organizations);
-
-    const title = "Our Partner Organizations";
-    res.render("organizations", { title, organizations });
-});
-
-app.get("/projects", async (req, res) => {
-    const projects = await getAllProjects();
-    console.log(projects);
-
-    const title = 'Service Projects';
-    res.render("projects", { title, projects });
-});
-
-
-app.get("/categories", async (req, res) => {
-    const categories = await getAllCategories();
-    console.log(categories);
-    
-    const title = 'Categories';
-    res.render('categories', {title, categories});
-});
-
-// Test route for 500 errors
-app.get('/test-error', (req, res, next) => {
-    const err = new Error('This is a test error');
-    err.status = 500;
-    next(err);
-});
+// Routes. Importo esto del archivo routes.js. Allí es donde controlo las rutas
+app.use(router);
 
 // Este middleware se ejecuta cuando ninguna ruta anterior coincide con la peticion del usuario
 app.use((req, res, next) => {
